c2dc006b5bbb7c93bcc814349cb224764ef6637b
Irina Kravcova <irina.kravcova@gmail.com>
2022-03-03 18:28:20 +0200

    Safety commit

---
+++ src/main/java/com/doapp/doApp/models/UserLogin.java
@@ -0,0 +1,17 @@
+package com.doapp.doApp.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserLogin {
+    private String username;
+    private String password;
+}
